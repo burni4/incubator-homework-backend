50fe444cd9345d879086cfae36ded340fb650920
burni4 <artemnarchyk@gmail.com>
2022-10-16 00:02:42 +0300

    fix

--- src/h01/hometask_01_videos_router.ts
+++ src/h01/hometask_01_videos_router.ts
@@ -2,7 +2,7 @@ import {Request, Response, Router} from "express";
 
 export  const ht_01_Router = Router({});
 
-export const videos: videoType[] = [];
+export let videos: videoType[] = [];
 
 const errorsMessages: errorsMessagesType = CreateErrorsMessages();
 // types +
