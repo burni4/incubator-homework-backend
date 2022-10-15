3fc3f9d41d87b2ede8a8729157877853ab83bffe
burni4 <artemnarchyk@gmail.com>
2022-10-15 22:16:57 +0300

    fields validation

--- src/h01/hometask_01_videos_router.ts
+++ src/h01/hometask_01_videos_router.ts
@@ -4,7 +4,9 @@ export  const ht_01_Router = Router({});
 
 export const videos: videoType[] = [];
 
+const errorsMessages: errorsMessagesType = CreateErrorsMessages();
 // types +
+
 type videoType = {
     "id": number,
     "title": string,
@@ -17,6 +19,9 @@ type videoType = {
 }
 
 enum Resolutions { P144, P240, P360, P480, P720, P1080, P1440, P2160 }
+type messageType = {message: string, field: string}
+type errorsMessagesType = { errorsMessages: messageType[]}
+
 // types -
 
 ht_01_Router.get('/', (req: Request, res: Response) => {
@@ -52,6 +57,12 @@ ht_01_Router.delete('/:id', (req: Request, res: Response) => {
 ht_01_Router.post('/', (req: Request, res: Response) => {
 
     let body = req.body;
+
+    if(!FieldsValidation(body)){
+        res.status(400).send(errorsMessages);
+        return;
+    }
+
     let createdDate = new Date();
 
     let newVideo: videoType = {
@@ -77,6 +88,13 @@ ht_01_Router.post('/', (req: Request, res: Response) => {
 
 ht_01_Router.put('/:id', (req: Request, res: Response) => {
 
+    let body = req.body;
+
+    if(!FieldsValidation(body)){
+        res.status(400).send(errorsMessages);
+        return;
+    }
+
     let video = videos.find(v => v.id === +req.params.id)
 
     if (video){
@@ -99,4 +117,47 @@ function between(min:number, max:number) {
     return Math.floor(
         Math.random() * (max - min) + min
     )
+}
+
+function AddMessage(field:string, message:string): void{
+
+    let errorMsg: messageType = {message: message, field: field};
+
+    errorsMessages.errorsMessages.push(errorMsg);
+
+}
+
+function ClearErrorsMessages(): void{
+    errorsMessages.errorsMessages = [];
+}
+
+function CreateErrorsMessages(): errorsMessagesType{
+
+    return { errorsMessages: []};
+
+}
+
+function FieldsValidation(body: any): boolean{
+
+    ClearErrorsMessages();
+
+    let validationPassed = true;
+
+    if (body.title === undefined || typeof (body.title) !== 'string' || body.title.length > 40){
+        AddMessage("title", "inputModel [Title] has incorrect values");
+        validationPassed = false;
+    }
+
+    if (body.author === undefined || typeof (body.author) !== 'string' || body.author.length > 20){
+        AddMessage("author", "inputModel [Author] has incorrect values");
+        validationPassed = false;
+    }
+
+    if (body.availableResolutions !== undefined && body.availableResolutions.length === 0){
+        AddMessage("availableResolutions", "inputModel [AvailableResolutions] has incorrect values");
+        validationPassed = false;
+    }
+
+    return validationPassed;
+
 }
