3aa28d36b931f48fe7d593a8de65a4aba5e29def
burni4 <artemnarchyk@gmail.com>
2022-10-15 17:51:41 +0300

    create REST API (POST)

--- src/h01/hometask_01_videos_router.ts
+++ src/h01/hometask_01_videos_router.ts
@@ -13,8 +13,10 @@ type videoType = {
     "minAgeRestriction": number,
     "createdAt": string,
     "publicationDate": string,
-    "availableResolutions": string[]
+    "availableResolutions": Resolutions[]
 }
+
+enum Resolutions { P144, P240, P360, P480, P720, P1080, P1440, P2160 }
 // types -
 
 ht_01_Router.get('/', (req: Request, res: Response) => {
@@ -46,3 +48,32 @@ ht_01_Router.delete('/:id', (req: Request, res: Response) => {
     }
     res.send(404);
 })
+
+ht_01_Router.post('/', (req: Request, res: Response) => {
+
+    let newVideo: videoType = {
+        id: between(0,10000),
+        title: "",
+        author: "",
+        canBeDownloaded: false,
+        minAgeRestriction: 0,
+        createdAt: new Date().toISOString(),
+        publicationDate: new Date().toISOString(),
+        availableResolutions: []
+    };
+
+    newVideo.title = "";
+    newVideo.author = "";
+    newVideo.availableResolutions = [];
+
+    videos.push(newVideo);
+
+    res.status(201).send(newVideo);
+
+})
+
+function between(min:number, max:number) {
+    return Math.floor(
+        Math.random() * (max - min) + min
+    )
+}
